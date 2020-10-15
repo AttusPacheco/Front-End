e69a6b9b4f0931f73418478d23a4916314384764
attuspacheco <attuspacheco@gmail.com>
2020-10-15 01:35:11 -0300

    FE-1 Inicio da implementaçaõ do segundo layout

---
+++ layout_2/public/accordion/js/accordion.js
@@ -0,0 +1,25 @@
+const accordionDataOptions = {
+    state: {
+        dataValue: 'data-state',
+        open: "open",
+        closed: "closed",
+    },
+};
+
+let accordions = document.getElementsByClassName("accordion");
+
+Array.from(accordions).forEach( function(accordion) {
+    accordion.addEventListener('click', function (){
+        let accordionStates = accordionDataOptions.state;
+        let accordionState = accordion.getAttribute(accordionStates.dataValue);
+        accordionState = accordionState === null || accordionState === "" ? accordionStates.closed : accordionState;
+        accordionState = (accordionState === accordionStates.open) || (accordionState === accordionStates.closed) ? accordionState : accordionStates.closed;
+
+        if (accordionState === accordionStates.open){
+            accordion.setAttribute(accordionStates.dataValue, accordionStates.closed);
+        }else{
+            accordion.setAttribute(accordionStates.dataValue, accordionStates.open);
+        }
+
+    })
+});
--- readme.md
+++ readme.md
@@ -2,6 +2,4 @@
 
 #### Projeto criado para criação de componentes para reutilização em outras aplicações
 
- - Todos os componentes criados até agora foram criados apenas com `HTML` e `CSS`
- 
- - Até então não foi utilizado `JS` porque estou querendo aprender mais e saber o que é possível criar utilizando apenas `HTML` e `CSS`, caso eu sinta necessidade da utilização de `JS` irei começar a utilizar e implementar novas funcionalidades.
+ - Todos os layouts criados até agora foram criados apenas com `HTML`, `CSS` e `JavaScript`
