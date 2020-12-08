e747b6fe10d695dd3d1969e644f90ebddea9f285
attuspacheco <attuspacheco@gmail.com>
2020-12-08 00:06:34 -0300

    FE - Correções de layout

--- layout_2/public/js/base.js
+++ layout_2/public/js/base.js
@@ -1,57 +1,70 @@
 // Section Banner
 
-let banner = document.querySelectorAll('.banner');
+const BANNERS = document.querySelectorAll('.banner');
 
-banner.forEach(function (item){
-    let prevImageButton = item.querySelector('.prev-image');
-    let nextImageButton = item.querySelector('.next-image');
+BANNERS.forEach(function (item){
     let images = item.querySelectorAll('.banner-image img');
-    let currentActiveImage = item.querySelector('.banner-image img[class*="active"]');
-    let lastActiveImage;
-    let activeImageKey = 0;
-    let imagesLength = images.length - 1;
 
-    if ((currentActiveImage === null || currentActiveImage.length === 0) && images.length > 0){
-        images[0].classList.add('active');
+    let banner = {
+        'prevImageButton': item.querySelector('.prev-image'),
+        'nextImageButton': item.querySelector('.next-image'),
+        'images': images,
+        'currentActiveImage': item.querySelector('.banner-image img[class*="active"]'),
+        'timer': 5000,
+        'nextImageTimer': 0,
+        'lastActiveImage': 0,
+        'activeImageKey': 0,
+        'imagesLength': images.length - 1,
+    }
+
+    banner.nextImageTimer = setTimer();
+
+    if ((banner.currentActiveImage === null || banner.currentActiveImage.length === 0) && images.length > 0){
+        banner.images[0].classList.add('active');
     }else{
-        images.forEach(function (image, key){
+        banner.images.forEach(function (image, key){
             if (image.classList.contains('.active')){
-                activeImageKey = key;
+                banner.activeImageKey = key;
                 return null;
             }
         });
     }
 
-    if (images.length > 0){
-        prevImageButton.addEventListener('click', function (){
-            lastActiveImage = activeImageKey;
-            activeImageKey = activeImageKey < 1 ? imagesLength : activeImageKey - 1;
-            currentActiveImage = activeImageKey;
-            changeCurrentActiveImage(images, lastActiveImage, currentActiveImage);
+    if (banner.images.length > 0){
+        banner.prevImageButton.addEventListener('click', function (){
+            banner.lastActiveImage = banner.activeImageKey;
+            banner.activeImageKey = banner.activeImageKey < 1 ? banner.imagesLength : banner.activeImageKey - 1;
+            banner.currentActiveImage = banner.activeImageKey;
+            changeCurrentActiveImage();
         });
 
-        nextImageButton.addEventListener('click', function (){
-            lastActiveImage = activeImageKey;
-            activeImageKey = activeImageKey < imagesLength ? activeImageKey + 1 : 0;
-            currentActiveImage = activeImageKey;
-            changeCurrentActiveImage(images, lastActiveImage, currentActiveImage);
+        banner.nextImageButton.addEventListener('click', function (){
+            banner.lastActiveImage = banner.activeImageKey;
+            banner.activeImageKey = banner.activeImageKey < banner.imagesLength ? banner.activeImageKey + 1 : 0;
+            banner.currentActiveImage = banner.activeImageKey;
+            changeCurrentActiveImage();
         });
+
     }
+
+    function changeCurrentActiveImage() {
+        clearTimeout(banner.nextImageTimer);
+
+        setTimeout(function () {
+            banner.images[banner.lastActiveImage].classList.remove('active');
+            banner.images[banner.currentActiveImage].classList.add('active');
+        }, 500)
+
+        banner.nextImageTimer = setTimer();
+    }
+
+    function setTimer() {
+        return setTimeout(function () {
+            return banner.nextImageButton.click();
+        }, banner.timer);
+    }
+
 });
-
-function changeCurrentActiveImage(images, lastImageKey, currentImageKey) {
-
-    images[lastImageKey].classList.add('prepare-rm-change');
-
-    setTimeout(function () {
-        images[lastImageKey].classList.remove('active');
-        images[currentImageKey].classList.add('prepare-add-change');
-        images[currentImageKey].classList.add('active');
-        setTimeout(function (){return images[lastImageKey].classList.remove('prepare-rm-change')}, 500);
-        setTimeout(function (){return images[currentImageKey].classList.remove('prepare-add-change')}, 500);
-    }, 500)
-
-}
 
 // Section Product Presentation
 let productPresentation = document.querySelectorAll('.product-presentation');
